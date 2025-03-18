41e4c13c72f6e9bc7f77fee42710c593681aef72
ChenSir <1210309835@qq.com>
2025-03-18 18:16:45 +0800

    add jwtutil

---
+++ src/main/java/org/example/Utils/Jwtutils.java
@@ -0,0 +1,10 @@
+package org.example.Utils;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Jwtutils {
+    public String generateToken() {
+        return "suer";
+    }
+}
