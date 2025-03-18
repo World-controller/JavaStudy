0a93a6c9185f15527e7bdcec7e7d82ee031fa799
ChenSir <1210309835@qq.com>
2025-03-18 18:33:17 +0800

    set Token

---
+++ src/main/java/org/example/Utils/TokenUtils.java
@@ -0,0 +1,10 @@
+package org.example.Utils;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TokenUtils {
+    public String getToken() {
+        return "Token";
+    }
+}
