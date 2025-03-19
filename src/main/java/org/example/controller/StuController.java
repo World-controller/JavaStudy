3fad73ff1b0969f365dcbbbc54c2bcb2c1ea0489
ChenSir <1210309835@qq.com>
2025-03-19 09:53:51 +0800

    add Teacher

--- src/main/java/org/example/controller/StuController.java
+++ src/main/java/org/example/controller/StuController.java
@@ -5,4 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StuController {
+        public int add(int a, int b) {
+            return a + b;
+        }
+
 }
